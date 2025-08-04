07140db2a55e77f8f767fddebf824b9dd4daf9ac
yasi-python <yasi-python@hotmail.com>
2025-08-04 08:44:15 +0000

    fix: رفع کامل خطاهای استایل، favicon و CORS و بهبود تولید خودکار خروجی

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,html}',
+    './api/**/*.{js,ts,jsx,tsx,html,php}',
+    './channelsData/**/*.{js,ts,jsx,tsx,html,php}',
+    './templates/**/*.{js,ts,jsx,tsx,html,php}',
+    './*.php',
+    './*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
